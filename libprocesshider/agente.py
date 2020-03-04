8d5697c826a01a6f1802aa0966fa2a16b86279f9
Javier <javier.sanchez-pascual@estudiante.uam.es>
2020-03-04 21:43:08 +0100

    1.8 Agente como demonio, funcionando

--- libprocesshider/agente.py
+++ libprocesshider/agente.py
@@ -29,158 +29,157 @@ audit_log = '.audit.log'
 		#print(event)
 
 
-
-
-###############################################
-#COMPROBAR USUARIO EXISTE
-if len(sys.argv) < 4 or sys.argv[2] != '-ip':
-	print('Número de argumentos incorrecto.\n'
-		'Sigue el siguiente esquema: ./agente username -ip IP,PORT [-s kill,execve,etc] [--restart]')
-	os._exit(0)
-
-usuario = sys.argv[1]
-if not os.path.isdir('/home/'+usuario):
-	print("El usuario '{}' NO existe. Prueba con otro.".format(usuario))
-	os._exit(0)
-
-###############################################
-#COMPROBAR QUE EL ACTIVO EXISTE
-if not os.path.isfile('/home/'+usuario+'/credenciales/credenciales.txt'):
-	print('EL ACTIVO DE ENGAÑO NO EXISTE. Por favor créalo.')
-	os._exit(0)
-
-ip_port = sys.argv[3].split(',')
-HOST = ip_port[0]
-PORT = int(ip_port[1])
-
-
-
-###############################################
-#BUCLE INFINITO PARA LA ACTIVACION DEL ACTIVO
-i = inotify.adapters.Inotify()
-i.add_watch('/home/'+usuario+'/credenciales')
-
-for event in i.event_gen(yield_nones=False):
-    (_, type_names, path, filename) = event
-
-    if filename == 'credenciales.txt' and path == '/home/'+usuario+'/credenciales':
-    	break
-
-
-
-################################################
-#CREACION FICHERO PARA COMPROBAR USUARIO ROOT CON is_sudo de processhider.c
-if not os.path.isfile('/etc/hola'):
-	os.system("touch /etc/hola")
-os.system("chmod 600 /etc/hola")
-
-
-
-#################################################
-# Funcion para reiniciar y reconfigurar auditd
-def restart_auditd():
-	os.system('systemctl stop auditd')
-	os.system('rm /home/'+usuario+'/'+audit_log)
-	with open('/etc/audit/auditd.conf', "w") as audit_conf:
-		audit_conf.write ('#\n# This file controls the configuration of the audit daemon\n#\n'
-			'local_events = yes\n'
-			'write_logs = yes\n'
-			'log_file = /home/'+usuario+'/'+audit_log+'\n'
-			'log_group = adm\n'
-			'log_format = ENRICHED\n'
-			'flush = INCREMENTAL_ASYNC\n'
-			'freq = 50\n'
-			'max_log_file = 8\n'
-			'num_logs = 5\n'
-			'priority_boost = 4\n'
-			'disp_qos = lossy\n'
-			'dispatcher = /sbin/audispd\n'
-			'name_format = NONE\n'
-			'##name = mydomain\n'
-			'max_log_file_action = ROTATE\n'
-			'space_left = 75\n'
-			'space_left_action = SYSLOG\n'
-			'verify_email = yes\n'
-			'action_mail_acct = root\n'
-			'admin_space_left = 50\n'
-			'admin_space_left_action = SUSPEND\n'
-			'disk_full_action = SUSPEND\n'
-			'disk_error_action = SUSPEND\n'
-			'use_libwrap = yes\n'
-			'##tcp_listen_port = 60\n'
-			'tcp_listen_queue = 5\n'
-			'tcp_max_per_addr = 1\n'
-			'##tcp_client_ports = 1024-65535\n'
-			'tcp_client_max_idle = 0\n'
-			'enable_krb5 = no\n'
-			'krb5_principal = auditd\n'
-			'##krb5_key_file = /etc/audit/audit.key\n'
-			'distribute_network = no\n')
-
-	os.system('touch /home/'+usuario+'/'+audit_log)
-	os.system('chmod 600 /home/'+usuario+'/'+audit_log)
-	with open('/etc/audit/rules.d/audit.rules', "w") as audit_rules:
-		audit_rules.write('## First rule - delete all\n'
-			'-D\n\n'
-			'## Increase the buffers to survive stress events.\n'
-			'## Make this bigger for busy systems\n'
-			'-b 8192\n\n'
-			'## This determine how long to wait in burst of events\n'
-			'--backlog_wait_time 0\n\n'
-			'## Set failure mode to syslog\n'
-			'-f 1\n\n')
-		syscalls = sys.argv[5].split(',')
-		for s in syscalls:
-			audit_rules.write('-a always,exit -F arch=b64 -S '+s+' -k '+s+'_rule -F uid='+usuario+'\n')
-
-	os.system('systemctl restart auditd')
-
-
-################################################
-# Aniadir syscalls a auditd
-def add_syscall():
-	os.system('systemctl stop auditd')
-	with open('/etc/audit/rules.d/audit.rules', "a") as audit_rules:
-		syscalls = sys.argv[5].split(',')
-		for s in syscalls:
-			audit_rules.write('-a always,exit -F arch=b64 -S '+s+' -k '+s+'_rule -F uid='+usuario+'\n')
-
-	os.system('systemctl restart auditd')
-
-
-################################################
-# Gestion de auditd segun argumentos de entrada
-if (len(sys.argv) == 7 and sys.argv[6] == '--restart' and sys.argv[4] == '-s') or not os.path.isfile('/home/'+usuario+'/'+audit_log):
-	restart_auditd()
-elif len (sys.argv) == 6 and sys.argv[4] == '-s':
-	add_syscall()
-
-
-
-###############################################
-#COMPILACION E INSERCION LIBRERIA DINAMICA
-os.system("make")
-os.system("mv libprocesshider.so /usr/local/lib/")
-os.system("echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload")
-
-
-
-##############################################
-#ESTRUCTURACION DIRECTORIO DE EVIDENCIAS
-hiddenD = "/etc/dpkg/origins/..."
-
-if not os.path.isdir(hiddenD):
-	os.system("mkdir "+hiddenD)
-
-hiddenD = hiddenD+'/'+usuario
-os.system("cp -r /home/{} {}".format(usuario, hiddenD))
-os.system("chmod 600 "+hiddenD)
-
-
-
-#########################################################
-# AGENTE DE ENGAÑO: monitoriza todos los eventos en el sistema de ficheros
 def agent():
+
+	###############################################
+	#COMPROBAR USUARIO EXISTE
+	if len(sys.argv) < 4 or sys.argv[2] != '-ip':
+		print('Número de argumentos incorrecto.\n'
+			'Sigue el siguiente esquema: ./agente username -ip IP,PORT [-s kill,execve,etc] [--restart]')
+		os._exit(0)
+
+	usuario = sys.argv[1]
+	if not os.path.isdir('/home/'+usuario):
+		print("El usuario '{}' NO existe. Prueba con otro.".format(usuario))
+		os._exit(0)
+
+	###############################################
+	#COMPROBAR QUE EL ACTIVO EXISTE
+	if not os.path.isfile('/home/'+usuario+'/credenciales/credenciales.txt'):
+		print('EL ACTIVO DE ENGAÑO NO EXISTE. Por favor créalo.')
+		os._exit(0)
+
+	ip_port = sys.argv[3].split(',')
+	HOST = ip_port[0]
+	PORT = int(ip_port[1])
+
+
+
+	###############################################
+	#BUCLE INFINITO PARA LA ACTIVACION DEL ACTIVO
+	i = inotify.adapters.Inotify()
+	i.add_watch('/home/'+usuario+'/credenciales')
+
+	for event in i.event_gen(yield_nones=False):
+	    (_, type_names, path, filename) = event
+
+	    if filename == 'credenciales.txt' and path == '/home/'+usuario+'/credenciales':
+	    	break
+
+
+
+	################################################
+	#CREACION FICHERO PARA COMPROBAR USUARIO ROOT CON is_sudo de processhider.c
+	if not os.path.isfile('/etc/hola'):
+		os.system("touch /etc/hola")
+	os.system("chmod 600 /etc/hola")
+
+
+
+	#################################################
+	# Funcion para reiniciar y reconfigurar auditd
+	def restart_auditd():
+		os.system('systemctl stop auditd')
+		os.system('rm /home/'+usuario+'/'+audit_log)
+		with open('/etc/audit/auditd.conf', "w") as audit_conf:
+			audit_conf.write ('#\n# This file controls the configuration of the audit daemon\n#\n'
+				'local_events = yes\n'
+				'write_logs = yes\n'
+				'log_file = /home/'+usuario+'/'+audit_log+'\n'
+				'log_group = adm\n'
+				'log_format = ENRICHED\n'
+				'flush = INCREMENTAL_ASYNC\n'
+				'freq = 50\n'
+				'max_log_file = 8\n'
+				'num_logs = 5\n'
+				'priority_boost = 4\n'
+				'disp_qos = lossy\n'
+				'dispatcher = /sbin/audispd\n'
+				'name_format = NONE\n'
+				'##name = mydomain\n'
+				'max_log_file_action = ROTATE\n'
+				'space_left = 75\n'
+				'space_left_action = SYSLOG\n'
+				'verify_email = yes\n'
+				'action_mail_acct = root\n'
+				'admin_space_left = 50\n'
+				'admin_space_left_action = SUSPEND\n'
+				'disk_full_action = SUSPEND\n'
+				'disk_error_action = SUSPEND\n'
+				'use_libwrap = yes\n'
+				'##tcp_listen_port = 60\n'
+				'tcp_listen_queue = 5\n'
+				'tcp_max_per_addr = 1\n'
+				'##tcp_client_ports = 1024-65535\n'
+				'tcp_client_max_idle = 0\n'
+				'enable_krb5 = no\n'
+				'krb5_principal = auditd\n'
+				'##krb5_key_file = /etc/audit/audit.key\n'
+				'distribute_network = no\n')
+
+		os.system('touch /home/'+usuario+'/'+audit_log)
+		os.system('chmod 600 /home/'+usuario+'/'+audit_log)
+		with open('/etc/audit/rules.d/audit.rules', "w") as audit_rules:
+			audit_rules.write('## First rule - delete all\n'
+				'-D\n\n'
+				'## Increase the buffers to survive stress events.\n'
+				'## Make this bigger for busy systems\n'
+				'-b 8192\n\n'
+				'## This determine how long to wait in burst of events\n'
+				'--backlog_wait_time 0\n\n'
+				'## Set failure mode to syslog\n'
+				'-f 1\n\n')
+			syscalls = sys.argv[5].split(',')
+			for s in syscalls:
+				audit_rules.write('-a always,exit -F arch=b64 -S '+s+' -k '+s+'_rule -F uid='+usuario+'\n')
+
+		os.system('systemctl restart auditd')
+
+
+	################################################
+	# Aniadir syscalls a auditd
+	def add_syscall():
+		os.system('systemctl stop auditd')
+		with open('/etc/audit/rules.d/audit.rules', "a") as audit_rules:
+			syscalls = sys.argv[5].split(',')
+			for s in syscalls:
+				audit_rules.write('-a always,exit -F arch=b64 -S '+s+' -k '+s+'_rule -F uid='+usuario+'\n')
+
+		os.system('systemctl restart auditd')
+
+
+	################################################
+	# Gestion de auditd segun argumentos de entrada
+	if (len(sys.argv) == 7 and sys.argv[6] == '--restart' and sys.argv[4] == '-s') or not os.path.isfile('/home/'+usuario+'/'+audit_log):
+		restart_auditd()
+	elif len (sys.argv) == 6 and sys.argv[4] == '-s':
+		add_syscall()
+
+
+
+	###############################################
+	#COMPILACION E INSERCION LIBRERIA DINAMICA
+	os.system("make")
+	os.system("mv libprocesshider.so /usr/local/lib/")
+	os.system("echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload")
+
+
+
+	##############################################
+	#ESTRUCTURACION DIRECTORIO DE EVIDENCIAS
+	hiddenD = "/etc/dpkg/origins/..."
+
+	if not os.path.isdir(hiddenD):
+		os.system("mkdir "+hiddenD)
+
+	hiddenD = hiddenD+'/'+usuario
+	os.system("cp -r /home/{} {}".format(usuario, hiddenD))
+	os.system("chmod 600 "+hiddenD)
+
+
+
+	#########################################################
+	# AGENTE DE ENGAÑO: monitoriza todos los eventos en el sistema de ficheros
 	# Create context for de TLS session
 	context = OpenSSL.SSL.Context(TLSv1_2_METHOD)
 
@@ -291,5 +290,5 @@ def agent():
 
 #############################################################
 # DEMONIZACION
-#with daemon.DaemonContext(stdout=sys.stdout):
-agent()
+with daemon.DaemonContext(stdin=sys.stdin, stdout=sys.stdout):
+	agent()
